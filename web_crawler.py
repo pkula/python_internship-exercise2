de00208c127ff2f240f1fb3a9e86d762112c9b67
pkula <px.kula@gmail.com>
2019-02-05 11:17:10 +0100

    function which add protocol

--- web_crawler.py
+++ web_crawler.py
@@ -1,7 +1,7 @@
 import urllib
 from bs4 import BeautifulSoup
 import lxml
-
+import sys
 
 def find_core_URL(URL):
     core_URL = ""
@@ -16,39 +16,42 @@ def find_core_URL(URL):
             core_URL = URL
     return core_URL
 
+def add_protocol(adress):
+    if adress[0:3] == "www":
+        return "http://" + adress
+    elif adress[0:7] == "http://" or adress[0:8] == "https://":
+        return adress
+    else:
+        return "http://www."+ adress
+
 
 def site_map(URL):
-    core_URL = find_core_URL(URL)
-    soup = BeautifulSoup(urllib.request.urlopen(URL).read(),"lxml")
-    title = (str(soup.find("title"))[7:-8])
-    links = []
-    for link in soup.find_all("a"):
-        link = link.get("href")
-        if link == "#":
-            continue
-        if link[0]=="/":
-            link = core_URL + link
-        if link[0:len(core_URL)] != core_URL:
-            continue
-        links.append(link)
+    try:
+        URL = add_protocol(URL)
+        core_URL = find_core_URL(URL)
+        soup = BeautifulSoup(urllib.request.urlopen(URL).read(),"lxml")
+        title = (str(soup.find("title"))[7:-8])
+        links = []
+        for link in soup.find_all("a"):
+            link = link.get("href")
+            if link == "#":
+                continue
+            if link[0]=="/":
+                link = core_URL + link
+            if link[0:len(core_URL)] != core_URL:
+                continue
+            links.append(link)
         
-    dictionary = {URL: {
-    "title":title,
-    "links":set(links)}
-    }
+        dictionary = {URL: {
+        "title":title,
+        "links":set(links)}
+        }
     
-    print(dictionary)
+        print(dictionary)
 
+    except:
+        print("critical error", file=sys.stderr)
 
-site_map("http://0.0.0.0:8000")
-print("\n\n")
-site_map("http://0.0.0.0:8000/site.html")
-print("\n\n")
-site_map("http://0.0.0.0:8000/example.html")
-print("\n\n")
-site_map("http://0.0.0.0:8000/site/subsite.html")
-print("\n\n")
-site_map("http://0.0.0.0:8000/site/other_site.html")
+site_map("krkmeble.pl")
 
-site_map("http://www.krkmeble.pl/")
 
